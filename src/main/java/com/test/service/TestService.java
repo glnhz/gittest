23ec370b72dfd73fcf0928ae2f7ee41cd24ab182
zhouylard <360775698@qq.com>
2017-09-28 17:10:57 +0800

    first commit

---
+++ src/main/java/com/test/service/TestService.java
@@ -0,0 +1,8 @@
+package com.test.service;
+
+public class TestService {
+
+	public static void main(String[] args) {
+		System.out.println("service");
+	}
+}
---
+++ src/main/java/com/test/web/TestController.java
@@ -0,0 +1,8 @@
+package com.test.web;
+
+public class TestController {
+
+	public static void main(String[] args) {
+		System.out.println("controller");
+	}
+}
